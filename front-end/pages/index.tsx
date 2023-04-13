01f004f030db926ec4dacafcb5833b501f40fe68
Zach Xu <zach.popping@gmail.com>
2023-04-13 15:32:37 -0400

    set up frontend

---
+++ front-end/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next'
+import React from 'react'
+
+type Props = {}
+
+const Index: NextPage = (props: Props) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Index
